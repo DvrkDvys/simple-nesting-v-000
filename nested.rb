6489859587fe7e2370f5f1f73d32ec1ddae484b9
Jordan Harris <jharris@wework.com>
2018-07-11 13:40:47 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -21,8 +21,7 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
-	programmer_hash = 
+ 	programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -107,7 +106,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-      programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     
      
 end
 
